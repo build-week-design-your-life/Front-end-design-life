e825f09be7d7b64dc84c20c8367e8800bad78472
Zolton <gmzesq@gmail.com>
2019-07-29 22:39:08 -0400

    Added netlify file

--- design-life/src/components/login/login.js
+++ design-life/src/components/login/login.js
@@ -1,37 +1,37 @@
-import React, {useState} from "react";
-import axiosWithAuth from "../security/AxiosWithAuth"
-import FormikLoginForm from "./registrationForm"
+// import React, {useState} from "react";
+// import axiosWithAuth from "../security/AxiosWithAuth"
+// import FormikLoginForm from "./registrationForm"
 
-// In testing - login form that assumes token is already set on local storage
+// // In testing - login form that assumes token is already set on local storage
 
 
-const Login = props => {
-    const [credentials, setCredentials] = useState({});
-    login = event => {
-        event.preventDefaul();
-        axiosWithAuth()
-        .get("URL", credentials)
-        .then(res=>{
-            //TAKE THIS OUT AFTER ITS WORKING
-            console.log(res)
-            localStorage.setItem("token", res.data.token)
-            props.history.push("/design")
-        })
-        .catch(rej => {
-            console.log(rej)
-            props.history.push("/")
-        })
-    }
-    return <FormikLoginForm />
-}
+// const Login = props => {
+//     const [credentials, setCredentials] = useState({});
+//     login = event => {
+//         event.preventDefaul();
+//         axiosWithAuth()
+//         .get("URL", credentials)
+//         .then(res=>{
+//             //TAKE THIS OUT AFTER ITS WORKING
+//             console.log(res)
+//             localStorage.setItem("token", res.data.token)
+//             props.history.push("/design")
+//         })
+//         .catch(rej => {
+//             console.log(rej)
+//             props.history.push("/")
+//         })
+//     }
+//     return <FormikLoginForm />
+// }
 
-const login = () => {
-    axiosWithAuth()
-        .post("URL", userCredentials)
-        .then(res=>{
-            localStorage.setItem("token", res.data.token)
-            props.history.push("/design")
-        })
-}
+// const login = () => {
+//     axiosWithAuth()
+//         .post("URL", userCredentials)
+//         .then(res=>{
+//             localStorage.setItem("token", res.data.token)
+//             props.history.push("/design")
+//         })
+// }
 
-export default Login
+// export default Login
