1fb5283654ad3fe98adccab93d28944c300a1cae
Zolton <gmzesq@gmail.com>
2019-07-30 17:27:07 -0400

    POST form complete, waiting on green light from backend to start testing

--- design-life/src/components/design/design.js
+++ design-life/src/components/design/design.js
@@ -1,34 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from "../security/AxiosWithAuth";
+import NewPostFormikForm from "./newPostForm"
 
 function Design() {
+
   const [userID, setUserID] = useState();
 
   useEffect(() => {
     setUserID(localStorage.getItem("userID"));
-    console.log("setting user ID from local storage");
-    console.log(userID);
+    // console.log("setting user ID from local storage");
+    // console.log(userID);
   }, [userID]);
 
   useEffect(() => {
     axiosWithAuth()
       .get(`https://hr-bw3.herokuapp.com/api/journals/weekly/${userID}`)
       .then(res => {
-        console.log("GET successful");
-        console.log(res);
+        // console.log("GET successful");
+        // console.log(res);
       })
       .catch(rej => {
-        console.log("GET rejected");
-        console.log(rej);
+        // console.log("GET rejected");
+        // console.log(rej);
       });
-  }, []);
+  }, [userID]);
 
   return (
-    <h1>
-      {/* {console.log(userData)} */}
-      Congratulations! If you're seeing this, then the login, private route, and
-      localstorage token setting are working!
-    </h1>
+    <>
+    <h2>
+      Hello!  Your daily and weekly posts are below
+    </h2>
+    {console.log("whats being passed to formik forms from design file")}
+    {console.log(userID)}
+    <NewPostFormikForm userID = {userID} />
+    </>
   );
 }
 
---
+++ design-life/src/components/design/newPostForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { withFormik, Field, Form } from "formik";
+import * as Yup from "yup";
+import axiosWithAuth from "../security/AxiosWithAuth";
+
+function NewPost({ values, errors, touched, isSubmitting }) {
+  return (
+    <Form>
+      <h1>What are your thoughts for today?</h1>
+      <label>Title</label>
+
+      <Field name="journal_title" type="text" className="journalTitle" />
+      <label>Dear Diary, </label>
+      <Field name="journal_content" type="text" className = "journalContent" />
+      <Field component="select" name="journal_type">
+        <option value="daily">Daily</option>
+        <option value="weekly">Weekly</option>
+      </Field>
+      <button type="submit">Submit</button>
+    </Form>
+  );
+}
+
+const NewPostFormikForm = withFormik({
+  mapPropsToValues({ journal_content, journal_title, journal_type, userID }) {
+    console.log("What Formik sees as userID");
+    console.log(typeof userID);
+    console.log("journal title");
+    console.log(journal_title);
+    console.log("userID after conversion")
+    console.log(typeof userID)
+    return {
+      //user_id: userID || "",
+      user_id: 1,
+      journal_content: journal_content || "",
+      journal_title: journal_title || "",
+      journal_type: journal_type || "daily",
+      journal_date: Date.now()
+    };
+  },
+
+  // validationSchema: Yup.object().shape({
+  //   username: Yup.string()
+  //     .required("A Login name is required")
+  //     .min(6, "A username must be at least 6 characters long"),
+  //   password: Yup.string()
+  //     .min(8, "A password must be at leat 8 characters long")
+  //     .required("A password is required to continue")
+  // }),
+
+  handleSubmit(values, { resetForm }) {
+    console.log("values being handed to axios");
+    console.log(values);
+    axiosWithAuth()
+      .post("https://hr-bw3.herokuapp.com/api/journals", values)
+      .then(res => {
+        console.log("user POST res");
+        console.log(res);
+        //props.history.push("/design");
+        resetForm();
+      })
+      .catch(reject => {
+        // TAKE THIS OUT AFTER ITS WORKING - SECURITY RISK
+        console.log("values being rejected by axios");
+        console.log(values);
+        console.log("axios post rejection");
+        console.log(reject);
+      });
+  }
+})(NewPost);
+
+export default NewPostFormikForm;
